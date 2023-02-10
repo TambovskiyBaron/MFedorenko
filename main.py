779e5e86c1e4fc2185043bbc9bc2117f7ffa374c
TambovskiyBaron <124936527+TambovskiyBaron@users.noreply.github.com>
2023-02-10 10:32:53 +0300

    Add files via upload
    10.02

---
+++ main.py
@@ -0,0 +1,32 @@
+a = [True, 786 , 3.14, 'text', 70.2]
+b = [123, 'text']
+print(a) 
+print(a[0])
+print(a[1:3])
+print(a[2:])
+print(b)
+print(a+b)
+print(a)
+print(a[0])
+print(b)
+print(a+b)
+test = "Hello, Python"
+print (test[0])
+print (test[0:5])
+print (test[4:10])
+print (test[0:14])
+print (test[7:])
+print (test[:5])
+print (test[:])
+print (test[-1])
+print (test[-1:-14])
+print (test[::2])
+print (test[::-1])
+print (test + "Beshbarmak")
+print (test[-1])
+my_dict = {}
+my_dict["country"] = "Mexico"
+print (my_dict["country"])
+another_dict = {"number": 23, 2: True, "my_list":[1,2,3]}
+print (another_dict.keys())
+print (another_dict.values())
