5cba6f4a5d4944efe81a5fe006f1c2c12fbacc82
TambovskiyBaron <124936527+TambovskiyBaron@users.noreply.github.com>
2023-02-21 13:42:08 +0300

    Д/З На 21.02

---
+++ dz a.py
@@ -0,0 +1,10 @@
+a = input()
+b = input()
+if a > b:
+    c = a - b
+else:
+    c = a + b
+    
+if a == b:
+    c = a
+print(c)
---
+++ dz b.py
@@ -0,0 +1,9 @@
+a = int(input())
+if a < -5:
+    print('Perviy')
+elif -5 <= a <= 5:
+    print('Vtoroy')
+elif a<= -5:
+    print('Tretiy')
+else:
+    print('Samiy Bolshoy')
