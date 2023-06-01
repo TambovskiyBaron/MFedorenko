9ecb343f606d4b8cb1c774d09b9b5ae0431379e0
TambovskiyBaron <124936527+TambovskiyBaron@users.noreply.github.com>
2023-06-01 15:12:17 +0300

    01.06 sezon

---
+++ main (32).py
@@ -0,0 +1,28 @@
+def season(nomer):
+    if nomer == 1:
+        return ("Zima")
+    if nomer == 2:
+        return ("Zima")
+    if nomer == 3:
+        return ("Vesna")
+    if nomer == 4:
+        return ("Vesna")
+    if nomer == 5:
+        return ("Vesna")
+    if nomer == 6:
+        return ("Leto")
+    if nomer == 7:
+        return ("Leto")
+    if nomer == 8:
+        return ("Leto")
+    if nomer == 9:
+        return ("Osen")
+    if nomer == 10:
+        return ("Osen")
+    if nomer == 11:
+        return ("Osen")
+    if nomer == 12:
+        return("Zima")
+
+a = season(input("Nomer"))
+print(a)
